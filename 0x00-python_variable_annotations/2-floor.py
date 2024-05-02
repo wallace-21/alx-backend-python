1d34e20fd86add5860d448636f2815be20022dfe
wallace <exnesswallace@gmail.com>
2024-05-02 14:41:28 +0200

    fixed pycodestyle

--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -3,6 +3,7 @@
 """ function that a float as an argument and returns the floor of the float"""
 from typing import Any
 
+
 def floor(n: float) -> int:
     """ it accepts a single parameter """
     return (int(n))
