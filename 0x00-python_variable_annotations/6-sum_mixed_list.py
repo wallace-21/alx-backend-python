746035577e03b11a18e7f0a323e089c4864d4345
wallace <exnesswallace@gmail.com>
2024-05-03 00:39:14 +0200

    a function that takes a list of int and floats and returns the sum

---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+"""import a module called typing"""
+from typing import List, Union
+
+
+def sum_mixed_list(mxd_lst: List[Union[float, int]]) -> float:
+    """ accepts a list that has integers and floats"""
+    total = 0.00
+    for numbers in mxd_lst:
+        total += numbers
+
+    return (total)
