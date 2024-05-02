847317fc21df277613bdf1f16c4dc8400c32458f
wallace <exnesswallace@gmail.com>
2024-05-02 12:05:03 +0200

    fixed pycodestyle

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -3,5 +3,6 @@
 """ function that takes a float a and a float b as
 arguments and returns their sum as a float """
 
-def add(a: float, b: float ) -> float:
+
+def add(a: float, b: float) -> float:
     return (a + b)
