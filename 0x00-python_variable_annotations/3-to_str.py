516c2d46a7dce227b90a8750660b441e9c2a8797
wallace <exnesswallace@gmail.com>
2024-05-02 14:47:22 +0200

    fixed pycodestyle and changed the type of n

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+
+""" fucntion that takes a float parameter and returns
+the string representation of the float"""
+
+
+def to_str(n: float) -> str:
+    """ it accpets a single parameter"""
+    return (str(n))
