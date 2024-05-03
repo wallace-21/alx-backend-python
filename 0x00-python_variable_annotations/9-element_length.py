26bfa2b73ff9188d2c8ab00acd73bea07ebee047
wallace <exnesswallace@gmail.com>
2024-05-03 05:54:54 +0200

    function parameters and return values with the appropriate types

---
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+"""function that returns a list"""
+from typing import Iterable, Sequence, List, Tuple
+
+def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
+    """ it acceepts a single list"""
+    return [(i, len(i)) for i in lst]
