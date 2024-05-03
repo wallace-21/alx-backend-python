2393cbd3ade9b717ad4f06c7a3360895191c7d69
wallace <exnesswallace@gmail.com>
2024-05-03 05:46:45 +0200

    removed zero

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -6,7 +6,7 @@ from typing import List, Union
 
 def sum_mixed_list(mxd_lst: List[Union[float, int]]) -> float:
     """ accepts a list that has integers and floats"""
-    total = 0.00
+    total = 0.0
     for numbers in mxd_lst:
         total += numbers
 
