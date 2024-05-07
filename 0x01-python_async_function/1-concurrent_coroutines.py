d26620bc62e82060f971b16dc64b8345ffaa60a3
wallace <exnesswallace@gmail.com>
2024-05-07 08:30:52 +0200

    made the return type list of floats

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -3,10 +3,13 @@
 """ import async module and a function wait_random"""
 
 import asyncio
+from typing import List
+
+
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n: int, max_delay: int) -> float:
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """
     function that returns a list of floats
 
