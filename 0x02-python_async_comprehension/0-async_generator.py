cd04016398b4c7e2d7313e1972e2e859650f32f7
wallace <exnesswallace@gmail.com>
2024-05-08 11:03:30 +0200

    added the correct type-annotation on the var

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -6,12 +6,13 @@ import asyncio
 import random
 from typing import AsyncGenerator
 
-async def async_generator() -> AsyncGenerator[float, None]:
+async def async_generator() -> float:
     """ a Async Generator
 
     Yield:
         random number between 0 and 10
     """
     for i in range(10):
+        rand: float = random.choice(range(0, 10))
         await asyncio.sleep(1)
-        yield random.choice(range(0, 10))
+        yield rand
