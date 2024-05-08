f113d056d4b8be8d0da1802d27ff8f4afa23168d
wallace <exnesswallace@gmail.com>
2024-05-08 10:48:49 +0200

    fucntion that's a async generator

---
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+""" import the asyncio and random module"""
+
+import asyncio
+import random
+
+
+async def async_generator() -> float:
+    """ a Async Generator
+
+    Yield:
+        random number between 0 and 10
+    """
+    for i in range(10):
+        await asyncio.sleep(0.3)
+        yield random.choice(range(0, 10))
