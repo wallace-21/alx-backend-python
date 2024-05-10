3e21e600fda2cdabb47285308e1c5415e8ca3d48
wallace <exnesswallace@gmail.com>
2024-05-10 17:41:16 +0200

    Create an asyncio.Task to execute the wait_random coroutine asynchronously.

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -6,6 +6,18 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 def task_wait_random(max_delay: int) -> None:
+    """
+    Create an asyncio.Task to execute the
+    wait_random coroutine asynchronously.
+
+    Parameters:
+        max_delay (int): The maximum delay for
+        the wait_random coroutine.
+
+    Returns:
+        asyncio.Task: A Task object representing
+        the execution of wait_random coroutine.
+    """
     task = asyncio.Task(wait_random(max_delay))
 
     return(task)
