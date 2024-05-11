6671521bff05ef38f0d7e9b728fa3c5165bc2680
wallace <exnesswallace@gmail.com>
2024-05-11 14:02:50 +0200

    Included the right return type

--- 0x02-python_async_comprehension/1-async_comprehension.py
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -3,16 +3,16 @@
 """import the asyncio and random"""
 import asyncio
 import random
-from typing import List
+from typing import Generator
 
 async_generator = __import__('0-async_generator').async_generator
 
 
-async def async_comprehension() -> List[float]:
+async def async_comprehension() -> Generator:
     """Coroutine to collect 10 random numbers using async comprehension.
 
     Returns:
         List of 10 random numbers.
     """
-    result: List[float] = [i async for i in async_generator()]
+    result: Generator[float, None, None]= [i async for i in async_generator()]
     return result
