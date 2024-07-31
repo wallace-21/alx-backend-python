b226fa8bda591072af411fe841175b00d7e3abc4
wallace <exnesswallace@gmail.com>
2024-07-31 19:46:47 +0200

    fixed pycodestyle

--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -3,6 +3,7 @@
 """function that returns a list"""
 from typing import Iterable, Sequence, List, Tuple
 
+
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
     """ it acceepts a single list"""
     return [(i, len(i)) for i in lst]
